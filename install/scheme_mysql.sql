615d10c32e53dda1b4a1bb4ac0fcdcc125641226
Tom Reynolds <tomreyn@users.noreply.github.com>
2015-02-05 21:54:41 +0100

    Updated (?) DB scheme

--- install/scheme_mysql.sql
+++ install/scheme_mysql.sql
@@ -1,3 +1,56 @@
+-- Host: localhost    Database: masterserver
+-- ------------------------------------------------------
+-- Server version	5.6.22-71.0-log
+
+--
+-- Table structure for table `glestgameplayerstats`
+--
+
+DROP TABLE IF EXISTS `glestgameplayerstats`;
+CREATE TABLE `glestgameplayerstats` (
+  `lasttime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `gameUUID` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `playerUUID` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `platform` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `factionIndex` int(11) NOT NULL,
+  `controlType` int(11) NOT NULL,
+  `resourceMultiplier` decimal(10,6) NOT NULL,
+  `factionTypeName` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `personalityType` int(11) NOT NULL,
+  `teamIndex` int(11) NOT NULL,
+  `wonGame` int(11) NOT NULL,
+  `killCount` int(11) NOT NULL,
+  `enemyKillCount` int(11) NOT NULL,
+  `deathCount` int(11) NOT NULL,
+  `unitsProducedCount` int(11) NOT NULL,
+  `resourceHarvestedCount` int(11) NOT NULL,
+  `playerName` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `quitBeforeGameEnd` int(11) NOT NULL,
+  `quitTime` int(11) NOT NULL,
+  KEY `gameUUID` (`gameUUID`),
+  KEY `playerUUID` (`playerUUID`),
+  KEY `platform` (`platform`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Table structure for table `glestgamestats`
+--
+
+DROP TABLE IF EXISTS `glestgamestats`;
+CREATE TABLE `glestgamestats` (
+  `lasttime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `gameUUID` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `tech` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `factionCount` int(11) NOT NULL,
+  `framesPlayed` int(11) NOT NULL,
+  `framesToCalculatePlaytime` int(11) NOT NULL,
+  `maxConcurrentUnitCount` int(11) NOT NULL,
+  `totalEndGameConcurrentUnitCount` int(11) NOT NULL,
+  `isHeadlessServer` int(11) NOT NULL,
+  KEY `gameUUID` (`gameUUID`),
+  KEY `framesToCalculatePlaytime` (`framesToCalculatePlaytime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 --
 -- Table structure for table `glestmaps`
 --
@@ -57,60 +110,9 @@ CREATE TABLE `glestserver` (
   `country` varchar(2) COLLATE utf8_unicode_ci NOT NULL,
   `status` int(11) NOT NULL DEFAULT '0',
   `gameUUID` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
-  KEY `lasttime` (`lasttime`)
-  KEY `gameUUID` (`gameUUID`)
+  KEY `lasttime` (`lasttime`),
+  KEY `gameUUID` (`gameUUID`),
   KEY `status` (`status`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Table structure for table `glestgamestats`
---
-
-DROP TABLE IF EXISTS `glestgamestats`;
-CREATE TABLE `glestgamestats` (
-  `lasttime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `gameUUID` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `tech` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `factionCount` int(11) NOT NULL,
-  `framesPlayed` int(11) NOT NULL,
-  `framesToCalculatePlaytime` int(11) NOT NULL,
-  `maxConcurrentUnitCount` int(11) NOT NULL,
-  `totalEndGameConcurrentUnitCount` int(11) NOT NULL,
-  `isHeadlessServer` int(11) NOT NULL,
-
-  KEY `gameUUID` (`gameUUID`)
-  KEY `framesToCalculatePlaytime` (`framesToCalculatePlaytime`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Table structure for table `glestgameplayerstats`
---
-
-DROP TABLE IF EXISTS `glestgameplayerstats`;
-CREATE TABLE `glestgameplayerstats` (
-  `lasttime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `gameUUID` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `playerUUID` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `platform` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `factionIndex` int(11) NOT NULL,
-  `controlType` int(11) NOT NULL,
-  `resourceMultiplier` DECIMAL(10,6) NOT NULL,  
-  `factionTypeName` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `personalityType` int(11) NOT NULL,
-  `teamIndex` int(11) NOT NULL,
-  `wonGame` int(11) NOT NULL,
-  `killCount` int(11) NOT NULL,
-  `enemyKillCount` int(11) NOT NULL,
-  `deathCount` int(11) NOT NULL,
-  `unitsProducedCount` int(11) NOT NULL,
-  `resourceHarvestedCount` int(11) NOT NULL,
-  `playerName` varchar(60) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `quitBeforeGameEnd` int(11) NOT NULL,
-  `quitTime` int(11) NOT NULL,
-
-  KEY `gameUUID` (`gameUUID`)
-  KEY `playerUUID` (`playerUUID`)
-  KEY `platform` (`platform`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
@@ -162,5 +164,3 @@ CREATE TABLE `recent_servers` (
   `players` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=550 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-
